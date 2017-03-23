3d685845b87ac4b994f90304988cc1b5cb6dedc5
Thomas Sauvajon <thomas.sauvajon@gmail.com>
2017-03-23 18:51:22 +0100

    es5 -> es6
    et un peu de lint parce que

--- REST.js
+++ REST.js
@@ -1,83 +1,118 @@
-var bcrypt = require('bcrypt');
-var mysql   = require("mysql");
+const bcrypt = require('bcrypt');
+const mysql   = require('mysql');
 
+const REST_ROUTER = (router,connection) => {
+    this.handleRoutes(router,connection);
+};
 
+REST_ROUTER.prototype.handleRoutes = (router,connection) => {
 
-function REST_ROUTER(router,connection) {
-    var self = this;
-    self.handleRoutes(router,connection);
+  //*****************
+  //*     users     *
+  //*****************
+  router.get('/users', (req,res) => {
+    let query = 'SELECT * FROM ??';
+    const table = ['users'];
+    query = mysql.format(query, table);
+    connection.query(query, (err,rows) => {
+      if(err) {
+        res.json({
+          'Error': true,
+          'Message': 'Error executing MySQL query',
+        });
+      } else {
+        res.json(rows);
+      }
+    });
+  });
+
+  router.post('/users', (req, res) => {
+    let query = 'INSERT INTO ?? (??, ??, ??, ??) VALUES (?, ?, ?, ?)';
+    const table = [
+      'users',
+      'email',
+      'password',
+      'lastname',
+      'firstname',
+      req.body.email,
+      req.body.password,
+      req.body.lastname,
+      req.body.firstname,
+    ];
+    query = mysql.format(query,table);
+    connection.query(query, (err,rows) => {
+      if(err) {
+        res.json({
+          'Error': true,
+          'Message': 'Error executing MySQL query',
+        });
+      } else {
+        res.json({
+          'Error': false,
+          'Message': 'User Added !'
+        });
+      }
+    });
+  });
+
+  router.get('/protected/users/:id', (req,res) => {
+    let query = 'SELECT * FROM ?? WHERE ??=?';
+    const table = ['users', 'id', req.params.id];
+    query = mysql.format(query,table);
+    connection.query(query, (err,rows) => {
+      if(err) {
+        res.json({
+          'Error': true,
+          'Message' : 'Error executing MySQL query',
+        });
+      } else {
+        res.json(rows[0]);
+      }
+    });
+  });
+
+  router.put('/users/:id', (req,res) => {
+    let query = 'UPDATE users SET email=?, lastname=?, firstname=?  WHERE id=?';
+    const table = [
+      req.body.email,
+      req.body.lastname,
+      req.body.firstname,
+      req.params.id,
+    ];
+    query = mysql.format(query,table);
+    connection.query(query, (err,rows) => {
+      if(err) {
+        res.json({
+          'Error': true,
+          'Message' : 'Error executing MySQL query',
+        });
+      } else {
+        res.json({
+          'Error': false,
+          'Message' : 'User updated',
+        });
+      }
+    });
+  });
+
+  router.delete('/protected/users/:id', (req, res) => {
+    let query = 'DELETE from ?? WHERE ??=?';
+    const table = ['users','id',req.params.id];
+    query = mysql.format(query,table);
+    connection.query(query, (err,rows) => {
+      if(err) {
+        res.json({
+          'Error': true,
+          'Message': 'Error executing MySQL query',
+        });
+      } else {
+        res.json({
+          'Error': false,
+          'Message': `user with id ${req.params.id} was deleted`,
+        });
+      }
+    });
+  });
 }
 
-REST_ROUTER.prototype.handleRoutes= function(router,connection) {
-
-//*****************
-//*     users     *
-//*****************
-router.get("/users", function(req,res){
-    var query = "SELECT * FROM ??";
-    var table = ["users"];
-    query = mysql.format(query,table);
-    connection.query(query,function(err,rows){
-        if(err) {
-            res.json({"Error" : true, "Message" : "Error executing MySQL query"});
-        } else {
-            res.json(rows);
-        }
-    });
-});
-
-router.post("/users",function(req,res){
-    var query = "INSERT INTO ?? (??,??,??,??) VALUES (?,?,?,?)";
-    var table = ["users","email","password","lastname","firstname",req.body.email, req.body.password,req.body.lastname,req.body.firstname];
-    query = mysql.format(query,table);
-    connection.query(query,function(err,rows){
-        if(err) {
-            res.json({"Error" : true, "Message" : "Error executing MySQL query"});
-        } else {
-            res.json({"Error" : false, "Message" : "User Added !"});
-        }
-    });
-});
-
-router.get("/protected/users/:id",function(req,res){
-    var query = "SELECT * FROM ?? WHERE ??=?";
-    var table = ["users","id",req.params.id];
-    query = mysql.format(query,table);
-    connection.query(query,function(err,rows){
-        if(err) {
-            res.json({"Error" : true, "Message" : "Error executing MySQL query"});
-        } else {
-            res.json(rows[0]);
-        }
-    });
-});
-
-router.put("/users/:id",function(req,res){
-    var query = "UPDATE users SET email = ?, lastname = ?, firstname = ?  WHERE id = ?";
-    var table = [req.body.email,req.body.lastname,req.body.firstname, req.params.id];
-
-    query = mysql.format(query,table);
-    connection.query(query,function(err,rows){
-        if(err) {
-            res.json({"Error" : true, "Message" : "Error executing MySQL query"});
-        } else {
-            res.json({"Error" : false, "Message" : "User Updated "});
-        }
-    });
-});
-
-
-router.delete("/protected/users/:id",function(req,res){
-    var query = "DELETE from ?? WHERE ??=?";
-    var table = ["users","id",req.params.id];
-    query = mysql.format(query,table);
-    connection.query(query,function(err,rows){
-        if(err) {
-            res.json({"Error" : true, "Message" : "Error executing MySQL query"});
-        } else {
-            res.json({"Error" : false, "Message" : "user with id "+req.params.id+" was deleted"});
-        }
-    });
-});
-}
 module.exports = REST_ROUTER;
--- server.js
+++ server.js
@@ -1,68 +1,61 @@
-var express = require("express");
-var mysql   = require("mysql");
-var bodyParser  = require("body-parser");
-var rest = require("./REST.js");
-var app  = express();
-var morgan = require('morgan')
+const express = require("express");
+const mysql   = require("mysql");
+const bodyParser  = require("body-parser");
+const morgan = require('morgan')
+const rest = require("./REST.js");
 
+const app  = express();
 
-
-function REST(){
-    var self = this;
-    self.connectMysql();
+const REST = () => {
+  this.connectMysql();
 };
 
-app.all('/*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", "X-Requested-With,     Content-Type");
-    next();
+app.all('/*', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+  next();
 });
 
+REST.prototype.connectMysql = () => {
+  const pool      =    mysql.createPool({
+    connectionLimit : 100, //the number of pre-load connection.
+    host     : '', // host of your mySql.
+    user     : '', // user of your mySql.
+    password : '', // Password of your mySql.
+    port     : '', // Port of your mySql.
+    database : '', // Name of yout database in mySql.
+    // socketPath:  "/Applications/MAMP/tmp/mysql/mysql.sock", //if you have a socketPath.
+    debug    :  false
+  });
+  pool.getConnection((err,connection) => {
+    if(err) {
+      this.stop(err);
+    } else {
+      this.configureExpress(connection);
+    }
+  });
+};
 
+REST.prototype.configureExpress = (connection) => {
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use(morgan('tiny'))
+    const router = express.Router();
+    app.use('/api', router);
+    const rest_router = new rest(router,connection);
+    this.startServer();
+}
 
-REST.prototype.connectMysql = function() {
-    var self = this;
-    var pool      =    mysql.createPool({
-        connectionLimit : 100, //the number of pre-load connection.
-        host     : '', // host of your mySql.
-        user     : '', // user of your mySql.
-        password : '', // Password of your mySql.
-        port : '', // Port of your mySql.
-        database : '', // Name of yout database in mySql.
-        // socketPath:  "/Applications/MAMP/tmp/mysql/mysql.sock", //if you have a socketPath.
-        debug    :  false
+REST.prototype.startServer = () => {
+    app.listen(3000, () => {
+        console.log("All right my friend, sky is the limit ! enjoy !");
     });
-    pool.getConnection(function(err,connection){
-        if(err) {
-          self.stop(err);
-        } else {
-          self.configureExpress(connection);
-        }
-    });
-}
+};
 
-REST.prototype.configureExpress = function(connection) {
-      var self = this;
-//      app.use(morgan('dev'));
-      app.use(bodyParser.urlencoded({ extended: true }));
-      app.use(bodyParser.json());
-      app.use(morgan('tiny'))
-      var router = express.Router();
-      app.use('/api', router);
-      var rest_router = new rest(router,connection);
-      self.startServer();
-}
-
-REST.prototype.startServer = function() {
-      app.listen(3000,function(){
-          console.log("All right my friend, sky is the limit ! enjoy !");
-      });
-}
-
-REST.prototype.stop = function(err) {
+REST.prototype.stop = (err) => {
     console.log("ISSUE WITH MYSQL n" + err);
     process.exit(1);
-}
+};
 
 new REST();
